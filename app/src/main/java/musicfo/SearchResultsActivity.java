899ecb1433ad81a28251164fdb194377019059c3
nimda <nimda@nimda.local>
2016-12-07 20:27:26 -0800

    added spotify json parsing and style for search results

--- app/src/main/java/musicfo/SearchResultsActivity.java
+++ app/src/main/java/musicfo/SearchResultsActivity.java
@@ -1,8 +1,11 @@
 package musicfo;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +15,17 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -20,6 +34,13 @@ public class SearchResultsActivity extends AppCompatActivity {
 
   LinearLayout searchResultsView;
   EventFinder eventFinder;
+  String previewURL = "";
+  boolean isURLLoading = true;
+  /**
+   * ATTENTION: This was auto-generated to implement the App Indexing API.
+   * See https://g.co/AppIndexing/AndroidStudio for more information.
+   */
+  private GoogleApiClient client;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +65,9 @@ public class SearchResultsActivity extends AppCompatActivity {
     }
 
     addResultsToScreen(eventFinder.allEvents);
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+    // See https://g.co/AppIndexing/AndroidStudio for more information.
+    client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
   }
 
   private void addResultsToScreen(HashMap<String, HashSet<String>> results) {
@@ -63,16 +87,31 @@ public class SearchResultsActivity extends AppCompatActivity {
 
     // Set up the various parts of the expandable view for use.
     TextView eventTitle = (TextView) eventView.findViewById(R.id.event_title);
+
     final LinearLayout expandableArtistList =
         (LinearLayout) eventView.findViewById(R.id.expandable_artist_list);
+
+    // Adds a left margin to the list of artists
+    ViewGroup.MarginLayoutParams params =
+        (ViewGroup.MarginLayoutParams) expandableArtistList.getLayoutParams();
+    params.leftMargin = 100;
+    expandableArtistList.setLayoutParams(params);
+
     final ImageButton toggleable = (ImageButton) eventView.findViewById(R.id.toggleable);
 
+    // Sets the size of the toggle button
+    ViewGroup.LayoutParams arrowParams = toggleable.getLayoutParams();
+    arrowParams.width = 60;
+    arrowParams.height = 60;
+    toggleable.setLayoutParams(arrowParams);
+
     // Set the title's text
-    eventTitle.setText(event);
-    eventTitle.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+    eventTitle.setText(event.replace("(", System.getProperty("line.separator") + "("));
+    eventTitle.setTextColor(Color.WHITE);
+    eventTitle.setTextSize(18);
 
     // Add a listener for the toggle
-    toggleable.setOnClickListener(new View.OnClickListener() {
+    eventTitle.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         if (expandableArtistList.getVisibility() == View.VISIBLE) {
@@ -87,12 +126,33 @@ public class SearchResultsActivity extends AppCompatActivity {
       }
     });
 
+
     // Add artist names to the expandable section.
+
     for (String artist : artistList) {
-      TextView artistTextView = new TextView(eventView.getContext());
+
+      final TextView artistTextView = new TextView(eventView.getContext());
+
       artistTextView.setText(artist);
+      artistTextView.setTextSize(18);
+      artistTextView.setTextColor(Color.WHITE);
+
+      artistTextView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          String a = ((TextView) v).getText().toString();
+          getSpotifyJSON(a);
+
+            System.out.println("uurrl:" + previewURL);
+
+
+
+        }
+      });
+
       expandableArtistList.addView(artistTextView);
     }
+
 
     return eventView;
   }
@@ -123,5 +183,113 @@ public class SearchResultsActivity extends AppCompatActivity {
   public void toggleViewExpansion(final View view) {
     LinearLayout layout = (LinearLayout) view.getParent();
 
+  }
+
+  //
+  // Given an Artist's Name
+  // Return a url to a 30s preview of their
+  // top track
+  //
+  public void getSpotifyJSON(String artist) {
+
+    //Searches Spotify's API for the artist
+
+    Ion.with(this)
+        .load("https://api.spotify.com/v1/search?q=" + artist.replaceAll(" ", "%20") + "&type=artist")
+        .asString()
+        .setCallback(new FutureCallback<String>() {
+          @Override
+          public void onCompleted(Exception e, String result) {
+
+            if (result != null) {
+              getPreviewURL(result);
+            }
+          }
+
+        });
+  }
+
+  public void getPreviewURL(String json) {
+
+    try {
+
+      JSONObject spotify_ret1 = new JSONObject(json);
+      JSONObject artists = spotify_ret1.getJSONObject("artists");
+      JSONArray items = artists.getJSONArray("items");
+      JSONObject mostPopularArtist = items.getJSONObject(0);
+      String artistID = mostPopularArtist.getString("id");
+
+
+      Ion.with(this)
+          .load("https://api.spotify.com/v1/artists/" + artistID + "/top-tracks?country=US")
+          .asString()
+          .setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+              if (result != null) {
+                parsePreviewURL(result);
+              }
+            }
+          });
+
+    } catch (JSONException e) {
+      e.printStackTrace();
+      Log.v("vvv", json);
+    }
+  }
+
+  public void parsePreviewURL(String json) {
+
+    try {
+
+      JSONObject spotify_ret2 = new JSONObject(json);
+
+      JSONArray tracks = spotify_ret2.getJSONArray("tracks");
+
+      JSONObject randomTrack = tracks.getJSONObject(1);
+
+      previewURL = randomTrack.getString("preview_url");
+      System.out.println(previewURL);
+      isURLLoading = false;
+
+    } catch (JSONException e) {
+      System.err.println(e.toString());
+    }
+  }
+
+  /**
+   * ATTENTION: This was auto-generated to implement the App Indexing API.
+   * See https://g.co/AppIndexing/AndroidStudio for more information.
+   */
+  public Action getIndexApiAction() {
+    Thing object = new Thing.Builder()
+        .setName("SearchResults Page") // TODO: Define a title for the content shown.
+        // TODO: Make sure this auto-generated URL is correct.
+        .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+        .build();
+    return new Action.Builder(Action.TYPE_VIEW)
+        .setObject(object)
+        .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+        .build();
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+    // See https://g.co/AppIndexing/AndroidStudio for more information.
+    client.connect();
+    AppIndex.AppIndexApi.start(client, getIndexApiAction());
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+    // See https://g.co/AppIndexing/AndroidStudio for more information.
+    AppIndex.AppIndexApi.end(client, getIndexApiAction());
+    client.disconnect();
   }
 }
