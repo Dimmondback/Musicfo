aa734b0797864890228473a0eeb0ce30478be2be
nimda <nimda@nimda.local>
2016-12-07 22:46:12 -0800

    tweaked the list of artists padding

--- app/src/main/java/musicfo/SavedEventsActivity.java
+++ app/src/main/java/musicfo/SavedEventsActivity.java
@@ -1,13 +1,85 @@
 package musicfo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class SavedEventsActivity extends AppCompatActivity {
+
+  ExpandableViewFactory expandableViewFactory;
+  LinearLayout searchResultsView;
+  EventFinder eventFinder;
+
+  /**
+   * ATTENTION: This was auto-generated to implement the App Indexing API.
+   * See https://g.co/AppIndexing/AndroidStudio for more information.
+   */
+  private GoogleApiClient client;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_saved_events);
+
+    searchResultsView = (LinearLayout) findViewById(R.id.search_results);
+    expandableViewFactory = new ExpandableViewFactory(this, searchResultsView);
+
+
+    expandableViewFactory.createExpandableView(eventFinder.allEvents);
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+    // See https://g.co/AppIndexing/AndroidStudio for more information.
+    client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int itemId = item.getItemId();
+    switch (itemId) {
+      case R.id.menu_saved_events:
+        Intent savedEvents = new Intent(getApplicationContext(), SavedEventsActivity.class);
+        startActivity(savedEvents);
+        return true;
+      case R.id.menu_settings:
+        Intent settings = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(settings);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+    // See https://g.co/AppIndexing/AndroidStudio for more information.
+    client.connect();
+    AppIndex.AppIndexApi.start(client, expandableViewFactory.getIndexApiAction());
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+
+    // ATTENTION: This was auto-generated to implement the App Indexing API.
+    // See https://g.co/AppIndexing/AndroidStudio for more information.
+    AppIndex.AppIndexApi.end(client, expandableViewFactory.getIndexApiAction());
+    client.disconnect();
   }
 }
